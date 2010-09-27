a0a1f780e9cd35c7b9a2c431def1dfc31f539d41
tattohonda <tattohonda@fd3afda3-cbae-de65-098d-fa821760f126>
2010-09-27 17:08:43 +0000

    Algumas funções de Round e KeyScheduler implementadas

--- Seguranca/src/pcs2055/curupira/Round.java
+++ Seguranca/src/pcs2055/curupira/Round.java
@@ -1,5 +1,7 @@
 package pcs2055.curupira;
 
+import pcs2055.math.ByteUtil;
+
 public class Round {
         
     public static byte[] initialKeyAddition(byte[] block, byte[] subkey) {
@@ -27,22 +29,32 @@ public class Round {
     
     private static byte[] gama(byte[] a) {
         
-        return null;
+    	return null;
     }
     
     private static byte[] pi(byte[] a) {
+    	
+    	int i, j, n;
+    	n = 4;
+    	byte[] result = a.clone();
+    	
+    	for (i = 1; i < 3; i++)
+    		for (j = 0; j < n; j++)
+    			result[i*n + j] = a[i*n + (i^j)];
         
-        return null;
+    	return result;
     }
     
     private static byte[] teta(byte[] a) {
-        
-        return null;
+
+    	int n = 4;
+    	byte[] MDS = {3, 2, 2, 4, 5, 4, 6, 6, 7};
+        return ByteUtil.mult3xn(MDS, a, n);	
     }
     
     private static byte[] sigma(byte[] a, byte[] subkey) {
         
-        return null;
+        return ByteUtil.xor(a, subkey, 12);
     }
 
 }
--- Seguranca/src/pcs2055/test/mathTest.java
+++ Seguranca/src/pcs2055/test/mathTest.java
@@ -1,5 +1,6 @@
 package pcs2055.test;
 
+import pcs2055.curupira.Round;
 import pcs2055.math.ByteUtil;
 
 public class mathTest {
@@ -9,7 +10,16 @@ public class mathTest {
         byte[] x = {1, 2, 3, 1, 2, 3};
         byte[] y = {1, 2, 3, 1, 2, 3};
         ByteUtil.mult3xn(x, y, 2);
+        
+        //byte[] a = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
+        //byte[] b = new byte[12];
+        //b = Round.gama(a); --> aparentemente ok
+        //ByteUtil.printArray(b);
 
+        //byte[] a = {0, 1, 2, 3, 4, 5, 0, 0, 0, 0, 0, 0};
+        //byte[] b = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
+        //byte[] c = Round.sigma(a, b);  --> aparentemente ok
+        //ByteUtil.printArray(c);
     }
 
 }
