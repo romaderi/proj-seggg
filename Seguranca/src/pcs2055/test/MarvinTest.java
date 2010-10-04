fe0942e055735c355e7a7ecd678901b914c90e85
leonardofl87 <leonardofl87@fd3afda3-cbae-de65-098d-fa821760f126>
2010-10-04 05:47:22 +0000

    polindo marvin

--- Seguranca/src/pcs2055/test/MarvinTest.java
+++ Seguranca/src/pcs2055/test/MarvinTest.java
@@ -44,6 +44,9 @@ public class MarvinTest {
         System.out.println("tag=");
         ByteUtil.printArray(tag);
         
+//        key = new byte[] {0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c};
+//        byte[] chunk = new byte[] {};
+        
 
     }
 
