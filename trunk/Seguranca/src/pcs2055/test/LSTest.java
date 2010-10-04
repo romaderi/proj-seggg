cc336fb736297009c7a29f65b93c44f2bcf695a3
leonardofl87 <leonardofl87@fd3afda3-cbae-de65-098d-fa821760f126>
2010-10-04 14:11:49 +0000

    Ciphertext do lettersoup funfando para (pelo menos) o primeiro exemplo

---
+++ trunk/Seguranca/src/pcs2055/test/LSTest.java
@@ -0,0 +1,36 @@
+package pcs2055.test;
+
+import pcs2055.curupira.Curupira;
+import pcs2055.interfaces.AED;
+import pcs2055.interfaces.BlockCipher;
+import pcs2055.interfaces.MAC;
+import pcs2055.ls.LetterSoup;
+import pcs2055.marvin.Marvin;
+import pcs2055.math.ByteUtil;
+
+public class LSTest {
+
+    public static void main(String[] args) {
+
+        byte[] plainText = new byte[23];
+        byte[] key = new byte[12];
+        byte[] nounce = new byte[12];
+        nounce[11] = 0x2A;
+        
+        BlockCipher curupira = new Curupira();
+        MAC marvin = new Marvin();
+        AED ls = new LetterSoup();
+        
+        ls.setKey(key, Curupira.KEY_SIZE_96);
+        ls.setCipher(curupira);
+        ls.setIV(nounce, 12);
+        ls.setMAC(marvin);
+
+        byte[] cData = new byte[12];
+        byte[] crypto = ls.encrypt(plainText, 23, cData);
+        
+        System.out.println("Ciphertext=");
+        ByteUtil.printArray(crypto);
+    }
+
+}
