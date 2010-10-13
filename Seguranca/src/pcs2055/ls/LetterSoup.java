f728d708e5cb0a35501b78eb2f3df596ccb5131a
leonardofl87 <leonardofl87@fd3afda3-cbae-de65-098d-fa821760f126>
2010-10-13 02:54:04 +0000

    Soh pra sincronizar...

--- Seguranca/src/pcs2055/ls/LetterSoup.java
+++ Seguranca/src/pcs2055/ls/LetterSoup.java
@@ -150,8 +150,8 @@ public class LetterSoup implements AED {
         
         int n = this.cipher.blockBits()/8; 
 
-        //byte[] A = new byte[tagBits/8];
-        byte[] A = new byte[12];
+        byte[] A = new byte[n];
+        ByteUtil.printArray(this.R, "R= ");
         this.mac.init(this.R);
         for (int i=0; i < this.autData.length; i+=n) {
             
@@ -165,6 +165,7 @@ public class LetterSoup implements AED {
             
         }
         
+        ByteUtil.printArray(A, "A= ");
         return A;
     }
 
@@ -177,7 +178,6 @@ public class LetterSoup implements AED {
         this.cipher.encrypt(ByteUtil.lpad(new byte[] {0}, 8*n), L); 
         
         // D <- ∗ (L, H, tau)  
-        //byte[] D = new byte[tagBits/8];
         byte[] D = new byte[n];
         this.mac.init(L);
         for (int i=0; i < this.H.length; i+=n) {
@@ -191,6 +191,7 @@ public class LetterSoup implements AED {
             this.mac.getTag(D, tagBits); 
         }
 
+        ByteUtil.printArray(D, "D= ");
         return D;
     }
 
--- Seguranca/src/pcs2055/test/LSTest.java
+++ Seguranca/src/pcs2055/test/LSTest.java
@@ -19,31 +19,74 @@ public class LSTest {
                 0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17};
         nounce[11] = 0x2A;
         
-        System.out.println("== 1o teste ==");
-        
-        test(msg, key, null, nounce, 12);
-
-        test(msg, key, header, nounce, 12);
+        System.out.println("** 1o teste **");
+        test(msg, key, null, nounce);
+        test(msg, key, header, nounce);
 
         ///////////////////////////
         
-        System.out.println("\n== #0-0 Set 1 - key = 0, msg = 0, add = null, nounce = 0 ==");
+        System.out.println("\n** #0-0 Set 1 - key = 0, msg = 0, add = null, nounce = 0 **");
         
         nounce = new byte[12];
         msg = new byte[1];
-        test3(msg, key, null, nounce);        
+        test(msg, key, null, nounce);        
+        msg = new byte[7];
+        test(msg, key, null, nounce);        
+        msg = new byte[8];
+        test(msg, key, null, nounce);      
+        msg = new byte[9];
+        test(msg, key, null, nounce);        
+        msg = new byte[11];
+        test(msg, key, null, nounce);        
+        msg = new byte[12];
+        test(msg, key, null, nounce);        
+        msg = new byte[13];
+        test(msg, key, null, nounce);        
+
+        ///////////////////////////
+
+        System.out.println("\n** #0-0 Set 2 - key = 0, msg != 0, add = null, nounce = 0 **");
         
+        msg = new byte[]{1};
+        test(msg, key, null, nounce);        
+        msg = new byte[]{1, 2, 3, 4, 5, 6, 7, 8};
+        test(msg, key, null, nounce);        
+        msg = new byte[]{1, 2, 3, 4, 5, 6, 7, 8, 9};
+        test(msg, key, null, nounce);        
+        msg = new byte[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 0xa, 0xb, 0xc};
+        test(msg, key, null, nounce);        
+        msg = new byte[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 0xa, 0xb, 0xc, 0xd};
+        test(msg, key, null, nounce);  
+        
+        System.out.println("\n** #0-0 Set 3 - key = 0, msg = 0, add = 0, nounce = 0 **");
+        
+        msg = new byte[1];
+        header = new byte[1];
+        test(msg, key, header, nounce);        
+
+//        System.out.println("\n** #0-0 Set 4 - key = 0, msg != 0, add != 0, nounce = 0 **");
+//
+//        msg = new byte[] {1};
+//        header = new byte[] {1};
+//        test(msg, key, header, nounce);        
+
+        System.out.println("\n** #0-1 Set 1 - key = 0, msg = 0, add = null, nounce != 0 **");
+
+        msg = new byte[1];
+        nounce = new byte[] {0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c};
+        test(msg, key, null, nounce);        
 
     }
-    
-    private static void test3(byte[] plainText, byte[] key, byte[] header, byte[] nounce) {
-    
-       test(plainText, key, header, nounce, 4);
-       test(plainText, key, header, nounce, 8);
-       test(plainText, key, header, nounce, 12);
-    }    
-    
-    private static void test(byte[] plainText, byte[] key, byte[] header, byte[] nounce, int tagbytes) {
+        
+    private static void test(byte[] plainText, byte[] key, byte[] header, byte[] nounce) {
+        
+        // input
+        
+        ByteUtil.printArray(key, "key= ");
+        ByteUtil.printArray(nounce, "nounce= ");
+        ByteUtil.printArray(plainText, "msg("+ plainText.length + ")= ");
+        
+        // init
         
         BlockCipher curupira = new Curupira();
         MAC marvin = new Marvin();
@@ -54,29 +97,35 @@ public class LSTest {
         ls.setIV(nounce, nounce.length);
         ls.setMAC(marvin);
 
-        ByteUtil.printArray(key, "key= ");
-        ByteUtil.printArray(nounce, "nounce= ");
-        ByteUtil.printArray(plainText, "msg= ");
-
+        // encrypt
+        
         byte[] cData = new byte[12]; // inútil aqui
         byte[] crypto = ls.encrypt(plainText, plainText.length, cData);
         
-        byte[] tag = ls.getTag(cData, tagbytes*8);
+        // authenticate
         
         if (header != null) {
             
             ByteUtil.printArray(header, "add= ");
             ls.update(header, header.length);
-            tag = ls.getTag(header, tagbytes*8);
         }
+        
+        byte[] tag4 = ls.getTag(cData, 4*8);
+        byte[] tag8 = ls.getTag(cData, 8*8);
+        byte[] tag12 = ls.getTag(cData, 12*8);
 
-        ByteUtil.printArray(crypto, "Ciphertext= ");
-        ByteUtil.printArray(tag, "Tag("+tagbytes+")= ");
+        // decrypt
 
         byte[] mData = new byte[plainText.length]; // inútil aqui
         byte[] decrypt = ls.decrypt(crypto, plainText.length, mData);
+
+        // output
         
+        ByteUtil.printArray(crypto, "Ciphertext= ");
         ByteUtil.printArray(decrypt, "PlainText= ");
+        ByteUtil.printArray(tag4, "Tag(4)= ");
+        ByteUtil.printArray(tag8, "Tag(8)= ");
+        ByteUtil.printArray(tag12, "Tag(12)= ");
         System.out.println("");
     }
     
