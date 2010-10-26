69bbd6e371581141089f628a317e82fb561be032
leonardofl87 <leonardofl87@fd3afda3-cbae-de65-098d-fa821760f126>
2010-10-26 22:53:55 +0000

    Iniciando parte 2!

---
+++ Seguranca/src/pcs2055/sponge/SpongeDuplex.java
@@ -0,0 +1,29 @@
+package pcs2055.sponge;
+
+public class SpongeDuplex implements Duplex {
+
+    @Override
+    public byte[] duplexing(byte[] sigma, int sigmaLength, byte[] z, int zLength) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public int getBitRate() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public int getCapacity() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public void init(int hashBits) {
+        // TODO Auto-generated method stub
+        
+    }
+
+}
---
+++ Seguranca/src/pcs2055/sponge/SpongePRNG.java
@@ -0,0 +1,41 @@
+package pcs2055.sponge;
+
+public class SpongePRNG implements SpongeRandom {
+
+    @Override
+    public void feed(byte[] sigma, int sigmaLength) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public byte[] fetch(byte[] z, int zLength) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void forget() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void getBitRate() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void getCapacity() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void init(int hashBits) {
+        // TODO Auto-generated method stub
+        
+    }
+
+}
