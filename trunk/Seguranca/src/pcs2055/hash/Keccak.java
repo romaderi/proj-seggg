735e65b918086a1bc93483900f8a569c5955f172
leonardofl87 <leonardofl87@fd3afda3-cbae-de65-098d-fa821760f126>
2010-10-26 22:53:55 +0000

    Iniciando parte 2!

---
+++ trunk/Seguranca/src/pcs2055/hash/Keccak.java
@@ -0,0 +1,23 @@
+package pcs2055.hash;
+
+public class Keccak implements HashFunction {
+
+    @Override
+    public byte[] getHash(byte[] val) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void init(int hashBits) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void update(byte[] aData, int aLength) {
+        // TODO Auto-generated method stub
+        
+    }
+
+}
---
+++ trunk/Seguranca/src/pcs2055/hash/KeccakF.java
@@ -0,0 +1,36 @@
+package pcs2055.hash;
+
+public class KeccakF {
+    
+    // colocar aqui constantes (segundo enunciado) que determinam número de rounds
+    
+    public static void f() {
+        
+    }
+    
+    private static void round() {
+        
+        // aqui vão as chamadas para os métodos de baixo
+    }
+    
+    private static void iota() {
+        
+    }
+    
+    private static void chi() {
+        
+    }
+    
+    private static void pi() {
+        
+    }
+    
+    private static void ro() {
+        
+    }
+    
+    private static void teta() {
+        
+    }
+
+}
