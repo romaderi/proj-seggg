b72baada84a7c5d4471c97f52699346454da0852
tattohonda <tattohonda@fd3afda3-cbae-de65-098d-fa821760f126>
2010-12-02 00:30:58 +0000

    parte3 revisado mas nao funcionando

--- trunk/Seguranca/src/pcs2055/asymmetric/CramerShoup.java
+++ trunk/Seguranca/src/pcs2055/asymmetric/CramerShoup.java
@@ -3,6 +3,7 @@ package pcs2055.asymmetric;
 import java.math.BigInteger;
 
 import pcs2055.hash.HashFunction;
+import pcs2055.math.ByteUtil;
 import pcs2055.sponge.SpongeRandom;
 
 public class CramerShoup implements KeyEncapsulation {
@@ -44,15 +45,19 @@ public class CramerShoup implements KeyEncapsulation {
         // calcula chaves privadas (quíntupla de inteiros módulo q)
         // (x1,x2,y1,y2,z) random values in [0, q-1]
         int zLength = PRIVATE_BITS_KEY_SIZE/8; 
-        byte[] zbuf = new byte[zLength]; 
+        byte[] zbuf = null; 
         zbuf = random.fetch(zbuf, zLength);
         x1 = new BigInteger(zbuf).mod(q);
+        zbuf = null; 
         zbuf = random.fetch(zbuf, zLength);
         x2 = new BigInteger(zbuf).mod(q);
+        zbuf = null; 
         zbuf = random.fetch(zbuf, zLength);
         y1 = new BigInteger(zbuf).mod(q);
+        zbuf = null; 
         zbuf = random.fetch(zbuf, zLength);
         y2 = new BigInteger(zbuf).mod(q);
+        zbuf = null; 
         zbuf = random.fetch(zbuf, zLength);
         z = new BigInteger(zbuf).mod(q);
         
@@ -72,6 +77,8 @@ public class CramerShoup implements KeyEncapsulation {
     @Override
     public BigInteger[] encrypt(BigInteger[] pk, byte[] m) {
 
+        byte[] zero = new byte[1];
+    	
         // chave pública fornecida
         BigInteger c = pk[0];
         BigInteger d = pk[1];
@@ -84,14 +91,15 @@ public class CramerShoup implements KeyEncapsulation {
         // encripta a chave simétrica m com a chave pública pk
         // u1 = g1^r , u2 = g2^r , e = h^r * m, α = H(u1 , u2 , e), v = c^r * d^(rα) 
         BigInteger u1 = g1.modPow(r, p); 
-        BigInteger u2 = g2.modPow(r, p); 
-        BigInteger mm = new BigInteger(m);
+        BigInteger u2 = g2.modPow(r, p);
+        BigInteger mm = new BigInteger(ByteUtil.append(zero, m, 1, m.length));
         BigInteger e = h.modPow(r, p).multiply(mm).mod(p); 
         hash.init(PRIVATE_BITS_KEY_SIZE); 
         hash.update(u1.toByteArray(), u1.toByteArray().length);
         hash.update(u2.toByteArray(), u2.toByteArray().length);
         hash.update(e.toByteArray(), e.toByteArray().length);
-        BigInteger alfa = new BigInteger(hash.getHash(null));
+        byte[] resume = hash.getHash(null);
+        BigInteger alfa = new BigInteger(ByteUtil.append(zero, resume, 1, resume.length));
         BigInteger v = c.modPow(r, p);
         BigInteger kalfa = r.multiply(alfa).mod(p);
         v = v.multiply(d.modPow(kalfa, p)).mod(p);
@@ -115,7 +123,9 @@ public class CramerShoup implements KeyEncapsulation {
         hash.update(u1.toByteArray(), u1.toByteArray().length);
         hash.update(u2.toByteArray(), u2.toByteArray().length);
         hash.update(e.toByteArray(), e.toByteArray().length);
-        BigInteger alfa = new BigInteger(hash.getHash(null));
+        byte[] zero = new byte[1];
+        byte[] resume = hash.getHash(null);
+        BigInteger alfa = new BigInteger(ByteUtil.append(zero, resume, 1, resume.length));
         BigInteger check = u1.modPow(x1, p);
         check = check.multiply(u2.modPow(x2, p)).mod(p);
         BigInteger factor = u1.modPow(y1, p);
@@ -127,7 +137,7 @@ public class CramerShoup implements KeyEncapsulation {
         
         // descriptografa e retorna a chave simétrica
         // m = e / (u1^z) mod p
-        BigInteger fac = u1.modPow(z, p).modInverse(p);
+        BigInteger fac = u1.modPow(q.subtract(z), p);
         BigInteger m = e.multiply(fac).mod(p);
         
         // convert to byte array, without the left-padding of “0” bits
